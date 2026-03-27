4b4c43317d04b2a65d22b3915da0183ac3ed8093
yonoyon <yonojouzu@mailbox.org>
2026-03-27 21:53:11 +0100

    Update listeditor.py
    cleaning up. will get to optimizing the input and stuff

--- listeditor.py
+++ listeditor.py
@@ -4,108 +4,84 @@ print("You can freely interact with a list using this program. Input 1 for viewi
 print("By inputting quit, you will always quit the progam, safely.")
 
 userinput = input("Input here: ")
-
 list1 = []
 
-def invalidinputmsg():
+def start(): #start program
+    startmsg()
+    check_fuin()
+
+def startmsg(): #start program msg
+    print("Starting program")
+def quitmsg(): #quit program msg
+    print("Exiting program... ")
+def invalidinputmsg(): #outputs invalid input message
     print("Invalid input. ")
 
-def quitmsg():
-    print("Exiting program... ")
-
-def start():
-    print("Starting... ")
-    checkfunc()
-
-
-def checkfunc():
+def check_fuin(): #first function that handles input at start - check_fi
     if userinput == "1":
-        func1()
+        view()
     elif userinput == "2":
-        func2()
+        check_fxe()
     elif userinput == "quit":
         quitmsg()
     else:
         invalidinputmsg()
-
-def func1():
+def view(): #for viewing, sends to handling editing func if needed - view?
     print(list1)
-    func1_ec = input("Would you like to edit this list? (y/n): ")
-    if func1_ec == "y":
-        func2()
-    elif func1_ec == "n":
+    view_ec = input("Would you like to edit this list? (y/n): ")
+    if view_ec == "y":
+        check_fxe()
+    elif view_ec == "n":
         quitmsg()
-
-def func2():
+def check_fxe(): #handling editing func, sends over to respective func #check_fxe
     if len(list1) == 0:
         print("List empty. ")
-        func2_input = int(input("Using random numbers to fill is recommended. Choosing them yourself is possible as well, input 1 or 2 respectively: "))
-        if func2_input == 1:
-            fillfunc1()
-        elif func2_input == 2:
-            fillfunc2()
+        check_fxe_input = int(input("Using random numbers to fill is recommended. Choosing them yourself is possible as well, input 1 or 2 respectively: "))
+        if check_fxe_input == 1:
+            fill_rndm()
+        elif check_fxe_input == 2:
+            fill_uin()
         else:
             invalidinputmsg()
     else:
-        editfunc()
+        edit()
 
-def fillfunc1():
+def fill_rndm(): #fills list w random numbers fill_rndm
     for i in range(25):
         x = random.randint(1, 99)
         list1.append(x)
-    print(list1)
-    editqfunc()
-
-
-def fillfunc2():
+    check_fxe()
+def fill_uin(): #uses input to fill list fill_uin
     listlen = int(input("Input length of list: "))
     if listlen < 0:
         invalidinputmsg()
     elif listlen == 0:
-        func2()
+        check_fxe()
     else:
         for i in range(listlen):
             list1.append(int(input("Input number at index ")))
-        func2()
+        check_fxe()
 
-def editfunc():
+def edit(): #first editing func, sends over to respective func edit
     print("List: " , list1)
-    editfunc_input = int(input("Add/Remove/Clear/Cancel (1/2/3/4)"))
-    if editfunc_input == 1:
-        editfunc_add()
-    elif editfunc_input == 2:
-        editfunc_remove()
-    elif editfunc_input == 3:
-        editfunc_clear()
+    edit_input = int(input("Add/Remove/Clear/Cancel (1/2/3/4)"))
+    if edit_input == 1:
+        edit_add()
+    elif edit_input == 2:
+        edit_remove()
+    elif edit_input == 3:
+        edit_clear()
     else:
         pass
-
-def editfunc_add():
+def edit_add(): #editing func for adding numbers (input) edit_add
     na = int(input("Input number to add: ")) 
     list1.append(na)
-    func2()
-
-def editfunc_remove():
+    check_fxe()
+def edit_remove(): #editing func for removing numbers, (last number) edit_remove
     list1.pop(len(list1) - 1)
-    func2()
-
-
-def editfunc_clear():
+    check_fxe()
+def edit_clear(): #editing func for clearing edit_clear
     list1.clear()
-    func2()
-
-def editqfunc():
-    editq = input("Edit/Quit (e/q): ")
-    if editq == "q" or editq == "quit":
-        quitmsg()
-    elif editq == "e":
-        func2()
-    else:
-        invalidinputmsg()
-
-
-
-
-
+    check_fxe()
 
 start()
